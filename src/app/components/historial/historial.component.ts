1eea22599882563fde75520924dc32852135d214
PaolaLpez <isapaola188@gmail.com>
2024-07-02 23:33:31 -0600

    paola cambios

---
+++ src/app/components/historial/historial.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial',
+  templateUrl: './historial.component.html',
+  styleUrl: './historial.component.css'
+})
+export class HistorialComponent {
+
+}
